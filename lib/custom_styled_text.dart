39696d8bfd2e7843ff57f0710470ee05906f776c
deveusopht <90627233+deveusopht@users.noreply.github.com>
2022-04-23 10:19:37 +0500

    initial commit

---
+++ lib/custom_styled_text.dart
@@ -0,0 +1,10 @@
+library custom_styled_text;
+
+class CustomStyledText {
+  final String text, bold;
+
+  CustomStyledText({
+    required this.text,
+    required this.bold,
+  });
+}
---
+++ test/custom_styled_text_test.dart
@@ -0,0 +1,8 @@
+import 'package:custom_styled_text/custom_styled_text.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = CustomStyledText(text: 'Abc', bold: '#');
+  });
+}
